d41ea7489fe5641eee61f8e7ea23d20acc5397fe
mims1234 <39203144+mims1234@users.noreply.github.com>
2018-06-17 17:14:58 +0530

    Add files via upload

---
+++ commands/delRole.js
@@ -0,0 +1,64 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message,args) => {
+
+    pin = 1
+    let Sender = message.member;
+    let user = message.author
+        let spaminterval = 0
+            if (user.r1spam) {
+                if (new Date().getTime() - user.r1spam < (spaminterval)*1000) {
+                    message.channel.send('Spam Alert! Wait for: ' + Math.floor(Math.round((spaminterval - (new Date().getTime() - user.r1spam) / 1000) * 100) / 100) + ' seconds')
+                    .then(msg => msg.delete(2000));
+                    return;
+                }
+                else { user.r1spam = new Date().getTime();
+                        setTimeout(function(){
+                            pin = 1
+                        },500)
+                    }
+            }
+            else { user.r1spam = new Date().getTime();
+                    setTimeout(function(){
+                        pin = 1;
+                    },500)
+                }
+    if(pin ===1)
+    {
+        code = args[1]
+        if(code === 'djrole') role = 'DJ Public'
+        if(code === 'rainbow') role = 'rainbow'
+        if(code === 'guest')
+        {
+            let modRole = message.guild.roles.find('name','Mod') 
+            if(!modRole) return message.channel.send('Bat Err')
+            if(message.member.roles.has(modRole.id))
+            {
+                role = 'Guest'
+            }
+            else return
+        } 
+        if(!code) return message.channel.send('has not mentioned role or doesnt have permission to add this role')
+        let rmember = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        //console.log(message.member)
+        if(!rmember) return message.reply("Couldn't find that user ");
+        let gRole = message.guild.roles.find(`name`,role);
+        if(!gRole) return message.reply("Couldn't find that role");
+
+        if(!rmember.roles.has(gRole.id)) return message.channel.send(`Doesn't have that ${role}.`);
+        await(rmember.removeRole(gRole.id));
+
+        try
+        {
+            await Sender.send(`${role} role removed to <@${rmember.id}> .`);
+        }
+        catch(e)
+        {       
+            message.channel.send(`${role} role removed to <@${rmember.id}> . (DM was Blocked)`);
+        }
+    }
+}
+
+module.exports.help = {
+    name : "delrole"
+}
