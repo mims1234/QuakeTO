9c30dd1dea6b4dcc239e5e4642127e1a5daac98b
mims1234 <39203144+mims1234@users.noreply.github.com>
2018-06-18 18:42:48 +0530

    Update addRole.js

--- commands/addRole.js
+++ commands/addRole.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message,args) => {
     pin = 1
     let Sender = message.member;
     let user = message.author
-        let spaminterval = 0
+        let spaminterval = 10
             if (user.r1spam) {
                 if (new Date().getTime() - user.r1spam < (spaminterval)*1000) {
                     message.channel.send('Spam Alert! Wait for: ' + Math.floor(Math.round((spaminterval - (new Date().getTime() - user.r1spam) / 1000) * 100) / 100) + ' seconds')
@@ -27,7 +27,7 @@ module.exports.run = async (bot,message,args) => {
     {
         code = args[1]
         if(code === 'djrole') role = 'DJ Public'
-        if(code === 'rainbow') role = 'rainbow'
+        if(code === 'rainbow') role = 'Rainbow'
         if(code === 'guest')
         {
             let modRole = message.guild.roles.find('name','Mod') 
@@ -47,9 +47,6 @@ module.exports.run = async (bot,message,args) => {
 
         if(rmember.roles.has(gRole.id)) return message.channel.send(`${role} has been already added.`);
         await(rmember.addRole(gRole.id));
-
-        //let reportschannel = message.guild.channels.find(`name` , "quakerole-log");
-        //if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
 
         try
         {
